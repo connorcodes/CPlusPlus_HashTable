f0fcad34f4e2a1d701d1e638731f8344bade6dcc
eldoradocoder <computerscientificprolific@gmail.com>
2018-12-08 22:40:43 -0800

    Removed lots of junk and updated formatting

--- hashTable.h
+++ hashTable.h
@@ -15,9 +15,10 @@
 class hashTable{
 
 private:
-    int table[SIZE], elementCount;
+    int table[SIZE]{}, elementCount;
 
 public:
+//Declarations
 
     //Initialize hash Table
     hashTable(){
@@ -27,195 +28,148 @@ public:
         elementCount = 0;
     }
 
-    //Displays the file
-    void writeOut(char * fileName)
-    {
-        std::ofstream out;
-
-        out.open(fileName);
-        if(!out)
-        {
-            std::cerr << "fail to open " << fileName << " for output!" << std::endl;
-            return;
-        }
-
-        int i;
-        char birthdate[100];
-        char name[100];
-       // node * curr;
-        for(i=0; i<capacity; i++) //TODO: What is capacity?
-        {
-            for(curr = table[i]; curr; curr = curr->next)
-            {
-                curr->item.getTickerSymbol (birthdate);
-                curr->item.getName (name);
-                out << birthdate << ';' << name << ';' << curr->item.getNetValue () << '\n';
-            }
-        }
-        out.close ();
-    }
-
-
-    //Read from textfile to hash Table
-    hashTable(char * fileName)
-    {
-        std::ifstream in;
-        data currData; //TODO: Replace 'data'
-        char birthdate[100];
-        char name[100];
-
-       // initializeTable();
-
-        in.open(fileName);
-        if(!in)
-        {
-            std::cerr << "fail to open " << fileName << " for input!" << std::endl;
-            return;
-        }
-
-        in.get(birthdate, 100, ';');
-        while(!in.eof())
-        {
-            in.ignore(100, ';');
-            in.get(name, 100, ';');
-            in.ignore(100, ';');
-
-            currData.setbirthdate (birthdate);
-            currData.setName (name);
-
-            insert(currData);
-
-            in.get(birthdate, 10, ';');
-        }
-        in.close ();
-    }
-
-
     // method that checks if the hash table is full or not
-    bool isFull(){
-        if(elementCount == SIZE){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
+    bool isFull();
 
     // method that returns position for a given element
-    int hashFunction(int element){
-        return element % SIZE;
-    }
+    int hashFunction(int element);
 
     // method that finds the next empty position using quadratic probing
-    int quadraticProbing(int element, int position){
+    int quadraticProbing(int element, int position);
+
+    // method that inserts element inside the hash table
+    void insert(int element);
+
+    // method that searches for an element in the table
+    // returns position of element if found
+    // else returns false
+    int search(int element);
+
+
+    // method to remove an element from the table
+    void remove(int element);
+
+    // method to display the hash table
+    void display();
+};
+
+//Public Definitions
+
+bool hashTable::isFull() {
+    if(elementCount == SIZE){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int hashTable::hashFunction(int element) {
+    return element % SIZE;
+}
+
+int hashTable::quadraticProbing(int element, int position) {
+    int limit = 50, i = 1;
+    int newPosition = -1;
+    while(i <= 50){
+        newPosition = this->hashFunction(position + (int)pow(i, 2));
+        if(this->table[newPosition] == 0){
+            break;
+        }
+        else{
+            i++;
+        }
+    }
+    return newPosition;
+}
+
+void hashTable::insert(int element) {  //TODO: change ints to strings
+    int position;
+    // checking if the table is full
+    if(this->isFull()){
+        std::cout << "\nHash Table Full";
+        return;
+    }
+
+    bool isStored = false;
+
+    position = this->hashFunction(element);
+
+    // checking if the position is empty
+    if(table[position] == 0){
+        table[position] = element;
+        std::cout << "\nElement " << element << " stored at position " << position;
+        isStored = true;
+        elementCount += 1;
+    }
+
+        // collision occured hence we do linear probing
+    else{
+        std::cout<<"\nCollision has occured for element " <<element<<" at position " <<position<<" finding new Position.";
+        position = quadraticProbing(element, position);
+        if(position != -1){
+            this->table[position] = element;
+            elementCount += 1;
+            std::cout<<"\nElement " <<element<<" stored at position " <<position;
+        }
+
+    }
+    return;
+}
+
+int hashTable::search(int element) {
+    bool found = false;
+
+    int position = this->hashFunction(element);
+
+    if(table[position] == element){
+        found = true;
+        return position;
+    }
+
+        // if element is not found at position returned hash function
+        // then we search element using quadratic probing
+    else{
         int limit = 50, i = 1;
         int newPosition = -1;
         while(i <= 50){
             newPosition = this->hashFunction(position + (int)pow(i, 2));
-            if(this->table[newPosition] == 0){
+            if(this->table[newPosition] == element){
+                found = true;
                 break;
             }
             else{
                 i++;
             }
         }
-        return newPosition;
-    }
-
-    // method that inserts element inside the hash table
-    void insert(int element){  //TODO: change ints to strings
-        int position;
-        // checking if the table is full
-        if(this->isFull()){
-            std::cout << "\nHash Table Full";
-            return;
+        if(found){
+            return newPosition;
         }
-
-        bool isStored = false;
-
-        position = this->hashFunction(element);
-
-        // checking if the position is empty
-        if(table[position] == 0){
-            table[position] = element;
-            std::cout << "\nElement " << element << " stored at position " << position;
-            isStored = true;
-            elementCount += 1;
-        }
-
-            // collision occured hence we do linear probing
         else{
-            std::cout<<"\nCollision has occured for element " <<element<<" at position " <<position<<" finding new Position.";
-            position = quadraticProbing(element, position);
-            if(position != -1){
-                this->table[position] = element;
-                elementCount += 1;
-                std::cout<<"\nElement " <<element<<" stored at position " <<position;
-            }
-
-        }
-        return;
-    }
-
-    // method that searches for an element in the table
-    // returns position of element if found
-    // else returns false
-    int search(int element){
-        bool found = false;
-
-        int position = this->hashFunction(element);
-
-        if(table[position] == element){
-            found = true;
-            return position;
-        }
-
-            // if element is not found at position returned hash function
-            // then we search element using quadratic probing
-        else{
-            int limit = 50, i = 1;
-            int newPosition = -1;
-            while(i <= 50){
-                newPosition = this->hashFunction(position + (int)pow(i, 2));
-                if(this->table[newPosition] == element){
-                    found = true;
-                    break;
-                }
-                else{
-                    i++;
-                }
-            }
-            if(found){
-                return newPosition;
-            }
-            else{
-                std::cout<<"Element not found";
-                return -1;
-            }
+            std::cout<<"Element not found";
+            return -1;
         }
     }
+}
 
-
-    // method to remove an element from the table
-    void remove(int element){
-        int position = this->search(element);
-        if(position != -1){
-            table[position] = 0;
-            std::cout<<"\nElement " <<element<<" is Deleted";
-            this->elementCount -= 1;
-        }else{
-            std::cout<<"\nElement is not present in the Hash Table";
-        }
-        return;
+void hashTable::display() {
+    for (int i = 0; i < SIZE; i++){
+        std::cout << "\n"<<i<<" = "<<table[i];
     }
+    std::cout<<"\nThe number of element is the Table are : " <<this->elementCount;
+}
 
-    // method to display the hash table
-    void display(){
-        for (int i = 0; i < SIZE; i++){
-            std::cout << "\n"<<i<<" = "<<table[i];
-        }
-        std::cout<<"\nThe number of element is the Table are : " <<this->elementCount;
+void hashTable::remove(int element) {
+    int position = this->search(element);
+    if(position != -1){
+        table[position] = 0;
+        std::cout<<"\nElement " <<element<<" is Deleted";
+        this->elementCount -= 1;
+    }else{
+        std::cout<<"\nElement is not present in the Hash Table";
     }
-};
+    return;
+}
+
+//End public definitions
 
 #endif //LAB6_CRS_HASHTABLE_H
--- main.cpp
+++ main.cpp
@@ -13,144 +13,16 @@
 
 using namespace std;
 
-void displayMenu();
-char getCommand();
-void executeCmd(char command, hashTable& aTable);
-
-void getName(data & stock); //TODO: change to bday and name
-int getInt(char * prompt);
-float getFloat(char * prompt);
-void getString(char * prompt, char * input);
-
-void display(const hashTable & aTable);
 
 int main(){
     //TODO: Allow program to read file from anywhere on host machine CHAR ERROR
     //TODO: Read bst_testfile.txt DONE
     //TODO: Convert yyyy-mm-dd to a single hash key number (string->int) MAYBE DO IN HASHTABLE.H?
 
-    int command = 1;
-    string filename;
 
-    cout << "Enter the file name: " << endl;
-    cin >> filename;
 
-    char fileName[] = "bst_testfile.txt"; //TODO: Add filename input question
-    hashTable birthdays(fileName);
-
-    displayMenu();
-    command = getCommand();
-    while(command != '5')
-    {
-        executeCmd(command, birthdays);
-        displayMenu();
-        command = getCommand();
-    }
-
-    birthdays.writeOut (fileName);
-    cout << "\nThank you for using The Birthdate Hashing System!" << endl << endl;
     return 0;
 }
-
-void displayMenu()
-{
-    cout << "\nWelcome to The Birthdate Hashing System! "<< endl;
-    cout << "\t1) add a birthdate" << endl
-         << "\t2) remove a birthdate" << endl
-
-         << "\t3) search for a birthdate" << endl
-         << "\t4) list all the birthdates" << endl
-         << "\t5) quit the application" << endl << endl;
-}
-
-char getCommand()
-{
-    int cmd;
-    cout << "Please enter your choice: ";
-    cin >> cmd;
-    cin.ignore(100, '\n');
-    return tolower(cmd);
-}
-
-void executeCmd(int command, hashTable& aTable)
-{
-    data stock; //TODO: change to bday and name
-    char key[MAX_LEN]; //TODO: What is MAX_LEN for?
-
-    switch(command)
-    {
-        case '1': getStock(stock);
-            aTable.insert (stock);
-            cout << endl << "The birthdate has been saved to the textfile. " << endl;
-            break;
-        case '2': getString("\nPlease enter the birthdate of the entry you want to remove: ", key);
-            aTable.remove(key);
-            cout << endl << key << " has been removed from the textfile. " << endl;
-            break;
-        case '3': getString("\nPlease enter the birthdate of the entry you want to search: ", key);
-            aTable.search (key, stock);
-            cout << endl << "Information about " << key << ": " << endl << '\t' << stock << endl;
-            break;
-        case '4': display(aTable);
-            break;
-        default: cout << "Wrong command! enter 1-4 for options or 5 to quit. " << endl;
-            break;
-    }
-}
-void display(const hashTable & aTable)
-{
-    aTable.display();
-}
-
-void getName(data & stock) //TODO: change to bday and name
-{
-    string bday[MAX_LEN]; //TODO: What is MAX_LEN for?
-    string name[MAX_LEN]; //TODO: What is MAX_LEN for?
-
-    cout << "\nPlease enter information about the stock: " << endl;
-    getString("\tBirthdate: ", bday);
-    getString("\tName: ", name);
-
-    stock.setBday (bday);
-    stock.setName (name);
-}
-int getInt(char * prompt)
-{
-    int temp;
-    cout << prompt;
-    cin >> temp;
-    while(!cin)
-    {
-        cin.clear ();
-        cin.ignore(100, '\n');
-        cout << "Illegal input -- try again: ";
-        cin >> temp;
-    }
-    cin.ignore(100, '\n');
-    return temp;
-}
-float getFloat(char * prompt)
-{
-    float temp;
-    cout << prompt;
-    cin >> temp;
-    while(!cin)
-    {
-        cin.clear ();
-        cin.ignore(100, '\n');
-        cout << "Illegal input -- try again: ";
-        cin >> temp;
-    }
-    cin.ignore(100, '\n');
-    return temp;
-}
-void getString(char * prompt, char * input)
-{
-    cout << prompt;
-    cin.get(input, MAX_LEN, '\n'); //TODO: What is MAX_LEN for?
-    cin.ignore (100, '\n');
-}
-
 
 /*
  * Hashing usage reference:
